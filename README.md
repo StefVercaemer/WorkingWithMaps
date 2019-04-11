3c71ada4893b38b29334a0a4b4aecee24f133c24
Stef Vercaemer <stef.vercaemer@spermalie.be>
2019-04-11 23:52:01 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,9 @@
+Working with Maps
+==============
+
+This sample relates to the [Working with Maps in Xamarin.Forms](http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/maps) doc.
+
+Author
+------
+
+Craig Dunn, Andrei Nitescu
---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkingWithMaps.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Xamarin.FormsMaps.Init("Am4-WsSh9-5v2vWH5ceEDF5nupf8ifwRmjuQ3CL9yZ3o7h2l81yqr9KIM7Jn3XpX");
+            this.LoadApplication(new WorkingWithMaps.App());
+        }
+    }
+}
